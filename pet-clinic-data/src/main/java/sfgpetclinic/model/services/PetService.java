27bc9d800d79572db30f3bca14fbba43bdd86e51
Badger42S <k3gbdia1@gmail.com>
2019-11-22 07:37:45 -0400

    mimic CRUD

--- pet-clinic-data/src/main/java/sfgpetclinic/model/services/PetService.java
+++ pet-clinic-data/src/main/java/sfgpetclinic/model/services/PetService.java
@@ -4,8 +4,5 @@ import sfgpetclinic.model.Pet;
 
 import java.util.Set;
 
-public interface PetService {
-    Pet findById (Long Id);
-    Set<Pet> findAll();
-    Pet save (Pet owner);
+public interface PetService extends CrudService<Pet, Long>{
 }
