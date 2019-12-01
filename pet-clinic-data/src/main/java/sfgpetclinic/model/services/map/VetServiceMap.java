3456d65d94e7d7394267e06d9b3a7da646a98d9b
Badger42S <k3gbdia1@gmail.com>
2019-12-01 18:57:44 -0400

    plug empty pages

--- pet-clinic-data/src/main/java/sfgpetclinic/model/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/sfgpetclinic/model/services/map/VetServiceMap.java
@@ -1,12 +1,20 @@
 package sfgpetclinic.model.services.map;
 
 import org.springframework.stereotype.Service;
+import sfgpetclinic.model.Speciality;
 import sfgpetclinic.model.Vet;
+import sfgpetclinic.model.services.SpecialitiesService;
 import sfgpetclinic.model.services.VetServise;
 
 import java.util.Set;
 @Service
 public class VetServiceMap extends AbstractMapService<Vet,Long> implements VetServise {
+    private final SpecialitiesService specialitiesService;
+
+    public VetServiceMap(SpecialitiesService specialitiesService) {
+        this.specialitiesService = specialitiesService;
+    }
+
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
@@ -19,6 +27,14 @@ public class VetServiceMap extends AbstractMapService<Vet,Long> implements VetSe
 
     @Override
     public Vet save(Vet saveObject) {
+        if(saveObject.getSpecialities().size()>0){
+            saveObject.getSpecialities().forEach(specialty->{
+                if(specialty.getId() == null){
+                    Speciality saveSpecialty = specialitiesService.save(specialty);
+                    specialty.setId(saveSpecialty.getId());
+                }
+            });
+        }
         return super.save(saveObject);
     }
 
--- pet-clinic-web/src/main/java/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/sfgpetclinic/controllers/VetController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import sfgpetclinic.model.services.VetServise;
-@RequestMapping("/vets")
+
 @Controller
 public class VetController {
     private final VetServise vetService;
@@ -13,7 +13,7 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"","/index","/index.html"})
+    @RequestMapping({"/vets","/vets/index","/vets/index.html","/vets.html"})
     public String listVets(Model model){
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
